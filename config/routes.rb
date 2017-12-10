90aaacde7d850d53944f23b87b2b170da0d6cda1
Hiatt Collins <hiatt.collins.github@gmail.com>
2017-12-10 15:39:22 -0600

    token authentication test files added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post 'authenticate', to: 'authentication#authenticate'
+
   resources :users, :queries, :text_inputs, :song_results
 
 end
